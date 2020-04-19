43c4d64cb42e3b4c9a4951fa11f82ada001bf115
Shasless <54817015+Shasless@users.noreply.github.com>
2020-04-19 19:03:13 +0200

    Update determinization.cpp

--- determinization.cpp
+++ determinization.cpp
@@ -21,10 +21,47 @@ void determinizationCompletion(Automate &ar) {
 }
 
 Automate determinizationCompletionAsynchronous(Automate &ar) {
-    return ar;
+    Automate ar2(ar);
+
+
+    ar2 =completion(ar2);
+    return ar2;
 }
 
 Automate completion(Automate &ar) {
+    bool verif= false;
+    state trash;
+    trash.terminal = false;
+    trash.initial = false;
+    trash.id = -1;
+    for (int i = 0; i < ar.alphabet_size - 1; i++) {
+        transition tmp;
+        tmp.state= -1;
+        tmp.character=ar.alphabet[i];
+        trash.transition_list.push_back(tmp);
+        }
+    ar.state_list.push_back(trash);
+    for (auto &i : ar.state_list) {
+        if(i.transition_list.size()!=ar.alphabet_size ){
+            for (int j = 0;j < ar.alphabet_size - 1; j++) {
+                for (auto &k : i.transition_list){
+                    if(ar.alphabet[j]==k.character){
+                        verif=true;
+                        break;
+                    }
+                }
+                if(!verif){
+                    transition tmp;
+                    tmp.state= -1;
+                    tmp.character=ar.alphabet[j];
+                    i.transition_list.push_back(tmp);
+
+                }
+                verif=false;
+            }
+        }
+    }
+
     return ar;
 }
 
